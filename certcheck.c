ad608aaa51eb5b351354a8236118fa7e005516f5
Arpit Bajaj <abajaj1@student.unimelb.edu.au>
2018-05-18 18:51:14 +0000

    Reading all files and using sample code

--- certcheck.c
+++ certcheck.c
@@ -25,22 +25,11 @@ int main(int argc, char **argv) {
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
-
-    while ((read = getline(&line, &len, input)) != -1) {
-       
-        char *original = malloc(strlen(line));
-
-        strcpy(original, line);
-        char *context;
-        char *type_delimitter = ",";
-        strtok_r(line, type_delimitter, &context);
-       
-        fputs(original, output);
-    }
+    char *test_cert_example;
+   
 
 
-
-    const char test_cert_example[] = "./cert-file2.pem";
+    //const char test_cert_example[] = "./cert-file2.pem";
     BIO *certificate_bio = NULL;
     X509 *cert = NULL;
     X509_NAME *cert_issuer = NULL;
@@ -51,67 +40,82 @@ int main(int argc, char **argv) {
     OpenSSL_add_all_algorithms();
     ERR_load_BIO_strings();
     ERR_load_crypto_strings();
+    while ((read = getline(&line, &len, input)) != -1) {
+       
+        char *original = malloc(strlen(line));
 
-    //create BIO object to read certificate
-    certificate_bio = BIO_new(BIO_s_file());
+        strcpy(original, line);
+        char *website;
+        char *type_delimitter = ",";
+        strtok_r(line, type_delimitter, &website);
+        test_cert_example = malloc(strlen(line));
+        strcpy(test_cert_example, line);
+        //printf("%s\n", line);
 
-    //Read certificate into BIO
-    if (!(BIO_read_filename(certificate_bio, test_cert_example)))
-    {
-        fprintf(stderr, "Error in reading cert BIO filename");
-        exit(EXIT_FAILURE);
-    }
-    if (!(cert = PEM_read_bio_X509(certificate_bio, NULL, 0, NULL)))
-    {
-        fprintf(stderr, "Error in loading certificate");
-        exit(EXIT_FAILURE);
-    }
-
-    //cert contains the x509 certificate and can be used to analyse the certificate
+        fputs(original, output);
     
-    //*********************
-    // Example code of accessing certificate values
-    //*********************
 
-    cert_issuer = X509_get_issuer_name(cert);
-    char issuer_cn[256] = "Issuer CN NOT FOUND";
-    X509_NAME_get_text_by_NID(cert_issuer, NID_commonName, issuer_cn, 256);
-    printf("Issuer CommonName:%s\n", issuer_cn);
+        //create BIO object to read certificate
+        certificate_bio = BIO_new(BIO_s_file());
 
-    //List of extensions available at https://www.openssl.org/docs/man1.1.0/crypto/X509_REVOKED_get0_extensions.html
-    //Need to check extension exists and is not null
-    X509_EXTENSION *ex = X509_get_ext(cert, X509_get_ext_by_NID(cert, NID_subject_key_identifier, -1));
-    ASN1_OBJECT *obj = X509_EXTENSION_get_object(ex);
-    char buff[1024];
-    OBJ_obj2txt(buff, 1024, obj, 0);
-    printf("Extension:%s\n", buff);
+        //Read certificate into BIO
+        if (!(BIO_read_filename(certificate_bio, test_cert_example)))
+        {
+            fprintf(stderr, "Error in reading cert BIO filename");
+            exit(EXIT_FAILURE);
+        }
+        if (!(cert = PEM_read_bio_X509(certificate_bio, NULL, 0, NULL)))
+        {
+            fprintf(stderr, "Error in loading certificate");
+            exit(EXIT_FAILURE);
+        }
 
-    BUF_MEM *bptr = NULL;
-    char *buf = NULL;
+        //cert contains the x509 certificate and can be used to analyse the certificate
+        
+        //*********************
+        // Example code of accessing certificate values
+        //*********************
 
-    BIO *bio = BIO_new(BIO_s_mem());
-    if (!X509V3_EXT_print(bio, ex, 0, 0))
-    {
-        fprintf(stderr, "Error in reading extensions");
+        cert_issuer = X509_get_issuer_name(cert);
+        char issuer_cn[256] = "Issuer CN NOT FOUND";
+        X509_NAME_get_text_by_NID(cert_issuer, NID_commonName, issuer_cn, 256);
+        printf("Issuer CommonName:%s\n", issuer_cn);
+
+        //List of extensions available at https://www.openssl.org/docs/man1.1.0/crypto/X509_REVOKED_get0_extensions.html
+        //Need to check extension exists and is not null
+        X509_EXTENSION *ex = X509_get_ext(cert, X509_get_ext_by_NID(cert, NID_subject_key_identifier, -1));
+        ASN1_OBJECT *obj = X509_EXTENSION_get_object(ex);
+        char buff[1024]; 
+        OBJ_obj2txt(buff, 1024, obj, 0);
+        printf("Extension:%s\n", buff);
+
+        BUF_MEM *bptr = NULL;
+        char *buf = NULL;
+
+        BIO *bio = BIO_new(BIO_s_mem());
+        if (!X509V3_EXT_print(bio, ex, 0, 0))
+        {
+            fprintf(stderr, "Error in reading extensions");
+        }
+        BIO_flush(bio);
+        BIO_get_mem_ptr(bio, &bptr);
+
+        //bptr->data is not NULL terminated - add null character
+        buf = (char *)malloc((bptr->length + 1) * sizeof(char));
+        memcpy(buf, bptr->data, bptr->length);
+        buf[bptr->length] = '\0';
+
+        //Can print or parse value
+        printf("%s\n", buf);
+
+        //*********************
+        // End of Example code
+        //*********************
+        X509_free(cert);
+        BIO_free_all(certificate_bio);
+        BIO_free_all(bio);
+        free(buf);
     }
-    BIO_flush(bio);
-    BIO_get_mem_ptr(bio, &bptr);
-
-    //bptr->data is not NULL terminated - add null character
-    buf = (char *)malloc((bptr->length + 1) * sizeof(char));
-    memcpy(buf, bptr->data, bptr->length);
-    buf[bptr->length] = '\0';
-
-    //Can print or parse value
-    printf("%s\n", buf);
-
-    //*********************
-    // End of Example code
-    //*********************
-
-    X509_free(cert);
-    BIO_free_all(certificate_bio);
-    BIO_free_all(bio);
-    free(buf);
+    
     exit(0);
 }
